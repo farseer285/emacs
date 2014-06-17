caf7bfade3ed493269de8ceee875704ac9049493
Neo Liu <farseer285@gmail.com>
2014-06-17 02:52:00 -0700

    Add conf in install.sh for xmodmap.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 ln -sf `pwd`/emacs ~/.emacs
 ln -sf `pwd`/emacs.d ~/.emacs.d
+ln -sf `pwd`/xmodmap ~/.xmodmap
+
+# Add "xmodmap ~/.xmodmap 2>/dev/null" to ~/.profile
 
 # for auto save
 mkdir -p ~/.emacs.d/.backup
